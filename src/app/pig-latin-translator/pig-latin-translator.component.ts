bb5da8ff78da68b4d940f2463e39f68d2f9dbfc8
Matt Anderson <him@backattheranch.ca>
2018-12-24 10:33:40 -0800

    Add pig latin form

--- src/app/pig-latin-translator/pig-latin-translator.component.ts
+++ src/app/pig-latin-translator/pig-latin-translator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pig-latin-translator',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pig-latin-translator.component.css']
 })
 export class PigLatinTranslatorComponent implements OnInit {
+  pigLatinForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.pigLatinForm = this.fb.group({
+      piglatin: ['']
+    });
   }
 
 }
